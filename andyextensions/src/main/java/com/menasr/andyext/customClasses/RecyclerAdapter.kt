e0d8b736ecc3f6ab38bb251f7031c07faafbd834
ranjandas503 <ranjan@techphant.com>
2020-01-31 19:04:06 +0530

    Ready for new version
    Added swipe gesture and item swap in custom recyclerview with and without lazy load.

--- andyextensions/src/main/java/com/menasr/andyext/customClasses/RecyclerAdapter.kt
+++ andyextensions/src/main/java/com/menasr/andyext/customClasses/RecyclerAdapter.kt
@@ -4,12 +4,70 @@ package com.menasr.andyext.customClasses
 
 import android.os.Handler
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.menasr.andyext.customClasses.listeners.ItemTouchHelperAdapter
+import com.menasr.andyext.customClasses.listeners.OnStartDragListener
+import com.menasr.andyext.customClasses.listeners.SimpleItemTouchHelperCallback
+import java.util.*
+import kotlin.collections.ArrayList
 
 abstract class RecyclerAdapter<MODEL_CLASS, VH_CLASS : RecyclerView.ViewHolder>(private val recyclerView: RecyclerView) :
-    RecyclerView.Adapter<VH_CLASS>() {
+    RecyclerView.Adapter<VH_CLASS>(), ItemTouchHelperAdapter, OnStartDragListener {
 
     private var list: MutableList<MODEL_CLASS> = ArrayList()
+
+    private var mItemTouchHelper: ItemTouchHelper? = null
+    private var cursorPosition: Int = 0
+
+    init {
+        canSwapOrDrag(false)
+    }
+
+    private fun setSwipeListener() {
+        val callback = SimpleItemTouchHelperCallback(this)
+        mItemTouchHelper = ItemTouchHelper(callback)
+        mItemTouchHelper?.attachToRecyclerView(recyclerView)
+    }
+
+    fun canSwapOrDrag(isSwappable: Boolean) {
+        if (isSwappable)
+            setSwipeListener()
+        else {
+            mItemTouchHelper = null
+            cursorPosition = 0
+        }
+    }
+
+    override fun onItemDrag(fromPosition: Int, toPosition: Int): Boolean {
+        //changing cursor position on dragging
+        cursorPosition = toPosition
+
+        //Do something on dragging two items
+        return if (onDragFromPosition(
+                list[fromPosition],
+                fromPosition,
+                list[toPosition],
+                toPosition
+            )
+        ) {
+            //swapping item
+            Collections.swap(list, fromPosition, toPosition)
+            notifyItemMoved(fromPosition, toPosition)
+            true
+        } else false
+    }
+
+    override fun onItemDismiss(position: Int, swipeDirection: Int) {
+        if (onSwipeRightOrLeft(list[position], position, swipeDirection)) {
+            list.removeAt(position)
+            notifyItemRemoved(position)
+        } else notifyItemChanged(position)
+    }
+
+    override fun onStartDrag(viewHolder: RecyclerView.ViewHolder) {
+        mItemTouchHelper?.startDrag(viewHolder)
+    }
 
     /**Get actual list*/
     fun getList() = list
@@ -54,5 +112,9 @@ abstract class RecyclerAdapter<MODEL_CLASS, VH_CLASS : RecyclerView.ViewHolder>(
 
     abstract fun onBindHolder(holder: VH_CLASS, data: MODEL_CLASS, position: Int)
     abstract fun onCreateHolder(parent: ViewGroup, viewType: Int): VH_CLASS
-
+    /**Return true if  you want to swap the position else return false*/
+    abstract fun onDragFromPosition(fromData: MODEL_CLASS, fromPosition: Int, toData: MODEL_CLASS, toPosition: Int): Boolean
+    /**Return true if  you want to delete the data in left or right swipe else return false.
+     * Check swipe direction as [ItemTouchHelper] is used for touch detection*/
+    abstract fun onSwipeRightOrLeft(data: MODEL_CLASS, position: Int, swipeDirection: Int): Boolean
 }
--- andyextensions/src/main/java/com/menasr/andyext/extensionFunctions/ViewUtils.kt
+++ andyextensions/src/main/java/com/menasr/andyext/extensionFunctions/ViewUtils.kt
@@ -4,6 +4,7 @@ package com.menasr.andyext.extensionFunctions
 
 import android.content.Context
 import android.graphics.drawable.GradientDrawable
+import android.view.MotionEvent
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.LinearLayout
@@ -46,5 +47,36 @@ fun View.showSoftKeyboard() {
     if (requestFocus()) {
         val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+    }
+}
+
+/**Set auto canShow of view, if the view is visible it will hide it or show it*/
+fun View.autoVisibility() = visibility(visibility == View.GONE)
+
+/**Reverse the visibility of view, pass your array of views*/
+fun autoVisibility(allViews: Array<View>) = allViews.iterator().forEach { it.autoVisibility() }
+
+/**Pass your array of allViews to hide the views*/
+fun hide(allViews: Array<View>) = allViews.iterator().forEach { it.hideView() }
+
+/**Pass your array of allViews to show the views*/
+fun show(allViews: Array<View>) = allViews.iterator().forEach { it.showView() }
+
+/**Set scroll gestures for layout or view inside a scrollview or scrolling view
+ * It uses OnTouchListener and MotionEvent*/
+fun View.forceScrollGestures() {
+    setOnTouchListener { v, event ->
+        when (event.action) {
+            MotionEvent.ACTION_DOWN ->
+                // Disallow ScrollView to intercept touch events.
+                v.parent.requestDisallowInterceptTouchEvent(true)
+
+            MotionEvent.ACTION_UP ->
+                // Allow ScrollView to intercept touch events.
+                v.parent.requestDisallowInterceptTouchEvent(false)
+        }
+
+        v.onTouchEvent(event)
+        true
     }
 }
